01d4211534ee81530449d9fe1dee2d28bc644121
Jiayi Hu <jiayi.ghu@gmail.com>
2021-02-26 16:27:51 +0100

    Generate samples from gaussian distribution

--- coap-utils/server-river.js
+++ coap-utils/server-river.js
@@ -1,9 +1,12 @@
 const coap = require('coap');
+const gaussian = require('gaussian');
 
 function reply(req, res, data) {
   if (req.headers['Observe'] !== 0) return res.end(data());
 
-  const interval = setInterval(() => res.write(data()), 3000);
+  res.write(data());
+
+  const interval = setInterval(() => res.write(data()), 3000 + Math.round(Math.random() * 3000));
 
   res.on('finish', (err) => clearInterval(interval));
 }
@@ -16,14 +19,30 @@ function spawnServer(listenCb) {
 
     console.log(`Request from ${req.rsinfo.address}:${req.rsinfo.port} for ${url}`);
 
+    res.setOption('Content-Format', 'text/plain');
+
     if (url.startsWith('/well-known/core')) {
       res.end(`
           </sensors/rainfall>;rt="rainfall";if="sensor",
           </sensors/flow>;rt="flow";if="sensor"`);
     } else if (url === '/sensors/rainfall') {
-      reply(req, res, () => String(Math.random() * 10));
+      const distribution = gaussian(0.05, 0.08);
+
+      reply(req, res, () => {
+        let sample = distribution.random(1)[0];
+        sample = sample < 0 ? -sample : sample;
+
+        return sample.toFixed(7);
+      });
     } else if (url === '/sensors/flow') {
-      reply(req, res, () => String(Math.random() * 100));
+      const distribution = gaussian(0.07, 0.09);
+
+      reply(req, res, () => {
+        let sample = distribution.random(1)[0];
+        sample = sample < 0 ? -sample : sample;
+
+        return sample.toFixed(7);
+      });
     } else {
       console.log(`Unknown resource ${url}`);
       res.code = '5.01';
